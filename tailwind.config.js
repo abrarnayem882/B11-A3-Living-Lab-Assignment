50eb28377f9d9aa954ddfb084b4fc46fd6992815
MD.ABU NAYEM <rajnayem882@gmail.com>
2025-02-01 20:33:36 +0600

    All done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {    keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }},
     },
     plugins: [],
   }
